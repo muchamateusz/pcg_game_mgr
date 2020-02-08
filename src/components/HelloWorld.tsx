9356c73372dd87302c4223a8c3875fbbbdc5e048
Mateusz Mucha <mucha.mateusz.inf@gmail.com>
2020-02-08 16:37:40 +0100

    react-phaser project is running

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+interface IHelloWorldProps {
+}
+
+const HelloWorld: React.FunctionComponent<IHelloWorldProps> = (props) => {
+  return ;
+};
+
+export default HelloWorld;
--- webpack/prod.js
+++ webpack/prod.js
@@ -1,27 +1,55 @@
-const merge = require("webpack-merge");
+// const merge = require("webpack-merge");
 const path = require("path");
-const base = require("./base");
-const TerserPlugin = require("terser-webpack-plugin");
-
-module.exports = merge(base, {
-  mode: "production",
+// const base = require("./base");
+// const TerserPlugin = require("terser-webpack-plugin");
+module.exports = {
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: "./src/index.js",
   output: {
-    filename: "bundle.min.js"
+    path: path.resolve("dist"),
+    filename: "index_bundle.js"
   },
-  devtool: false,
-  performance: {
-    maxEntrypointSize: 900000,
-    maxAssetSize: 900000
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false
-          }
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
-      })
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: "raw-loader"
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: "file-loader"
+      }
     ]
-  }
-});
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"], {
+      root: path.resolve(__dirname, "../")
+    }),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true)
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+      inject: "body"
+    })
+  ]
+};
