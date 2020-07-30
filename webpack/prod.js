d12c3340797d989a8c948fb89f7a22804874327f
Mateusz <34740182+muchamateusz@users.noreply.github.com>
2020-07-30 02:48:39 +0200

    webpack prod configuration changes - checking prod version ver1.1

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,58 +1,85 @@
-// const merge = require("webpack-merge");
 const path = require("path");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const base = require("./base");
-// const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: "production",
   entry: "./src/index.js",
   output: {
-    path: path.resolve("dist"),
-    filename: "index_bundle.js"
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/",
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    hot: true,
+    watchContentBase: true,
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: "babel-loader",
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
+        use: "raw-loader",
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
-      }
-    ]
+        use: "file-loader",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
+      root: path.resolve(__dirname, "../"),
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true)
+      WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "index.html",
       filename: "index.html",
-      inject: "body"
-    })
-  ]
+      inject: "body",
+    }),
+    new CircularDependencyPlugin({
+      exclude: /a\.js|node_modules/,
+      failOnError: false,
+      cwd: process.cwd(),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public/assets", to: "public/assets" }],
+    }),
+  ],
 };
