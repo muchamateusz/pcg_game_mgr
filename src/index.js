9f627bbce224b1ea304e2a4e70ed47c79dc0510c
Mateusz <34740182+muchamateusz@users.noreply.github.com>
2020-08-03 21:38:36 +0200

    risky commit just to check

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import Engine from "./classes/Engine";
 import TitleScreen from "./classes/TitleScreen";
 import { globals, config } from "./commons/globalVariables";
 import "./index.css";
-
+if (process.env.NODE_ENV !== 'production') {
+   console.log('Looks like we are in development mode!');
+}
 window.onload = () => {
   const game = new Phaser.Game(config);
   game.scene.add("title_screen", new TitleScreen(globals));
---
+++ webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Production",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
